855479bffbc4998010f9003a3dbdf94e3fd49eec
MikkeL0730 <57572232+MikkeL0730@users.noreply.github.com>
2023-03-24 14:21:47 +0400

    Add files via upload

--- laba2.py
+++ laba2.py
@@ -4,21 +4,19 @@ def multiple_replace(target_str, replace_values):
     return target_str
 import re
 file = open("test2.txt", "r")
-k = int(input('Введите количество цифр'))
+k = 5
 last_int_max = int(0)
 print('Натуральные числа, у которых более', k, 'цифр:')
 char = file.read()
 file.close()
-all = re.findall(r'\b\d+\b', char)
+all = re.findall(r'\b\d{5,}\b', char)
 symbolall = 0
 for i in range(len(all)):
-    symbol2 = len(all[i])
-    if (symbol2 > k):
-        chislo = int(all[i])
-        if (chislo > last_int_max):
-            last_int_max = chislo
-        symbolall += 1
-        print(all[i])
+    chislo = int(all[i])
+    if (chislo > last_int_max):
+        last_int_max = chislo
+    symbolall += 1
+    print(all[i])
 print("Всего чисел, содержащих более",k,"цифр -", symbolall,"\n","\nМаксимальное число -", last_int_max)
 slovar = {"0": "ноль ", "1": "один ", "2": "два ", "3": "три ", "4": "четыре ", "5": "пять ", "6": "шесть ", "7": "семь ", "8": "восемь ", "9": "девять "}
 print(multiple_replace(str(last_int_max), slovar))
